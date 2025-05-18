569559ffb262bf29082e25a96c7ff70398aaaccc
Ankit6055 <ankit.kumarrrrr6055@gmail.com>
2025-05-18 13:27:44 +0530

    Solve first pattern problem

--- Pattern/01.js
+++ Pattern/01.js
@@ -1,8 +1,16 @@
-let rows = 4;
+const prompt = require("prompt-sync")();
 
-for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < rows; j++) {
-        process.stdout.write("*")
+function printPattern(n) {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      process.stdout.write("*");
     }
-    process.stdout.write("\n")
+    process.stdout.write("\n");
+  }
+}
+
+let testCases = prompt("Enter the testcases: ");
+for (let i = 0; i < testCases; i++) {
+  const rows = prompt("Enter your rows: ");
+  printPattern(rows);
 }
